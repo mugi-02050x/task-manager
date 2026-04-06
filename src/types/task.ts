43f01491e8d407a397840548d17fc2b965c2dba2
mugi-02050x <109018697+mugi-02050x@users.noreply.github.com>
2026-04-06 16:02:10 +0900

    add: types

---
+++ src/types/task.ts
@@ -0,0 +1,78 @@
+// Main
+type TaskStatus = "WAITING" | "WORKING" | "COMPLETED";
+
+type Task = {
+  id: string;
+  taskName: string;
+  description: string;
+  status: TaskStatus;
+  parentId: string | null;
+  dispOrder: number;
+  createdAt: Date;
+};
+
+type TaskTrackRecord = {
+  id: string;
+  taskId: string;
+  startDatetime: Date;
+  endDatetime: Date;
+};
+
+// State
+type TaskState = {
+  tasks: Array<Task>;
+  trackRecords: Array<TaskTrackRecord>;
+};
+
+// Action
+type AddTaskAction = {
+  type: "ADD_TASK";
+  payload: Task;
+};
+
+type UpdateTaskAction = {
+  type: "UPDATE_TASK";
+  payload: Task;
+};
+
+type DeleteTaskAction = {
+  type: "DELETE_TASK";
+  payload: string; // タスクID
+};
+
+type ChangeStatusAction = {
+  type: "CHANGE_STATUS";
+  payload: { id: string; status: TaskStatus };
+};
+
+type AddTrackRecordAction = {
+  type: "ADD_TRACK_RECORD";
+  payload: TaskTrackRecord;
+};
+
+type DeleteTrackRecordAction = {
+  type: "DELETE_TRACK_RECORD";
+  payload: string; // 実績レコードID
+};
+
+type ChangeEndDateAction = {
+  type: "CHANGE_END_DATETIME";
+  payload: { id: string; endDatetime: Date };
+};
+
+type ImportAction = {
+  type: "IMPORT";
+  payload: TaskState;
+};
+
+type TaskAction =
+  | AddTaskAction
+  | UpdateTaskAction
+  | DeleteTaskAction
+  | ChangeStatusAction
+  | AddTrackRecordAction
+  | ChangeEndDateAction
+  | DeleteTrackRecordAction
+  | ImportAction;
+
+export type { TaskStatus, Task, TaskTrackRecord, TaskState, TaskAction };
